962097bea1f93f094f10c9fbde99dca1e5841238
rzadp <przemek@ethworks.io>
2019-05-14 15:12:37 +0200

    Updated e2e script.

--- ui-e2e/run-tests.sh
+++ ui-e2e/run-tests.sh
@@ -2,9 +2,8 @@
 cd $(dirname $0)
 
 docker-compose up -d --build --force-recreate
-cd ../bridge-ui
-npm run start:blocks &
-cd ../ui-e2e
+node ./scripts/blocks.js &
+
 docker-compose run contracts ./deploy.sh
 docker-compose run -d bridge npm run watcher:signature-request
 docker-compose run -d bridge npm run watcher:collected-signatures
@@ -20,10 +19,9 @@ docker-compose run -d bridge npm run sender:foreign
 docker-compose run -d ui npm start
 docker-compose run -d ui-erc20 npm start
 docker-compose run -d ui-erc20-native npm start
-cd ../bridge-ui
-npm run startE2e
+
+yarn mocha -b ./test.js
 rc=$?
-cd ../ui-e2e
 ps | grep node | grep -v grep | awk '{print "kill " $1}' | sh
 docker-compose down
 exit $rc
